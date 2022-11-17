f33173b7ab91c5a6cefd52377814f340893ca949
Wajd Aljaber <74684120+wajdsaleh@users.noreply.github.com>
2022-11-17 03:56:46 +0300

    Update database.php

--- php/database.php
+++ php/database.php
@@ -1,21 +1,9 @@
 <?php
-$conn = oci_connect("wajd", "wajd1200", "LAPTOP-FBNS74CI/XE");
+$conn = oci_connect("YourDBName", "YourDBPassword", "TheHostName");
 
 if (!$conn) {
     $e = oci_error();
     trigger_error(htmlentities($e['message']), E_USER_ERROR);}
-
-
-
-
-
-//         $val = "SELECT UNI_E_MAIL FROM REGISTER WHERE UPPER(UNI_E_MAIL) = UPPER (:email)";
-//         $R = oci_parse($conn,$val);
-//         oci_bind_by_name($R,"email",$email);    
-//         oci_execute($R);
-//         while($row = oci_fetch_array($R));
-//         if(oci_num_rows($R) != 0)
-//         die("Email is already used..");
 
 return $conn;
 
