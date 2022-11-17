a2d9720236cadc8f60e3e974e7b392d4bca73d0e
Wajd Aljaber <74684120+wajdsaleh@users.noreply.github.com>
2022-11-17 04:00:31 +0300

    Update search.php

--- php/search.php
+++ php/search.php
@@ -91,7 +91,7 @@
       </form>
 
 <?php
-    $conn = oci_connect("wajd", "wajd1200", "LAPTOP-FBNS74CI/XE");
+    $conn = oci_connect("YourDBName", "YourDBPassword", "TheHostName");
 
     if (!$conn)
     echo "not connected";
