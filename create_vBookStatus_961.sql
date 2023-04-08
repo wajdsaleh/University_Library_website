0e160370db9d129199d2bec867779cec8c064cdc
Wajd Aljaber <74684120+wajdsaleh@users.noreply.github.com>
2023-04-08 12:18:38 +0300

    Delete create_vBookStatus_961.sql

--- create_vBookStatus_961.sql
+++
@@ -1,4 +0,0 @@
-create view vBookStatus as 
-select ISSN,title,Is_it_borrowed as book_state , author_name
-from Book , Authors ,PublisherAuthor
-where PublisherAuthor.PA_ID = Book.PA_ID and PublisherAuthor.Author_id = Authors.Author_id;
